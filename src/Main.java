970844ce6085144525f01fe685d1dc2792af5f1f
freddycooper <almosttanonymous@gmail.com>
2024-04-01 03:56:19 +0300

    The homework has been completed.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Exercise 1
+        int[] newArray = new int[]{1, 2, 3};
+        double[] doubleArray = new double[]{1.57, 7.654, 9.986};
+        char[] charArray = {'a', 'b', 'c'};
+        // Exercise 2
+        System.out.println(Arrays.toString(newArray));
+        System.out.println(Arrays.toString(doubleArray));
+        System.out.println(Arrays.toString(charArray));
+        // Exercise 3
+        for (int index = 2; index >= 0; index--) {
+            System.out.print(newArray[index] + " ");
+        }
+        System.out.println();
+        for (int index = 2; index >= 0; index--) {
+            System.out.print(doubleArray[index] + " ");
+        }
+        System.out.println();
+        for (int index = 2; index >= 0; index--) {
+            System.out.print(charArray[index] + " ");
+        }
+        System.out.println();
+        // Exercise 4
+        for (int i = 0; i < newArray.length; i++) {
+            if (newArray[i] % 2 != 0) {
+                newArray[i] = newArray[i] + 1;
+            }
+            System.out.print(newArray[i] + " ");
+        }
     }
 }
